6d3bd1e3d6f3694ee3b84ab167cc49b56722986a
Lauren G <72687468+LaurenG123@users.noreply.github.com>
2023-10-02 16:19:52 +0100

    Json dataframe extraction as a function
    JSON extraction as a function that can be called

--- Extracting_JSON.py
+++ Extracting_JSON.py
@@ -1,34 +1,38 @@
 import boto3
 import json
 import pandas as pd
+def json_df():
 
-s3_client = boto3.client('s3')
-s3_resource = boto3.resource('s3')
+    s3_client = boto3.client('s3')
+    s3_resource = boto3.resource('s3')
 
-bucket_name = 'data-249-final-project'
-directory_prefix = 'Talent/'
+    bucket_name = 'data-249-final-project'
+    directory_prefix = 'Talent/'
+    df_array = []
 
-try:
-    response = s3_client.list_objects(Bucket=bucket_name, Prefix=directory_prefix)
-    if 'Contents' in response:
-        json_objects = [obj for obj in response['Contents'] if obj['Key'].endswith('.json')]
-        for json_object in json_objects:
-            response = s3_resource.Object(bucket_name, json_object['Key']).get()
-            json_content = response['Body'].read()
-            data = json.loads(json_content)
+    try:
+        response = s3_client.list_objects(Bucket=bucket_name, Prefix=directory_prefix)
+        if 'Contents' in response:
+            json_objects = [obj for obj in response['Contents'] if obj['Key'].endswith('.json')]
+            for json_object in json_objects:
+                response = s3_resource.Object(bucket_name, json_object['Key']).get()
+                json_content = response['Body'].read()
 
-            if isinstance(data, list):  # Check if the JSON data is a list of dictionaries
-                df = pd.DataFrame(data)
-                print(df)
-            elif isinstance(data, dict):  # Check if the JSON data is a single dictionary
-                df = pd.DataFrame([data])
-                print(df)
-            else:
-                print(f"Unsupported JSON format in '{json_object['Key']}'")
+                data = json.loads(json_content)
+                df_array.append(data)
 
-    else:
-        print(f"No objects found in the '{directory_prefix}' directory.")
-except Exception as e:
-    print(f"Error: {e}")
+                if isinstance(data, list):  # Check if the JSON data is a list of dictionaries
+                    df = pd.DataFrame(data)
+                    #print(df)
+                elif isinstance(data, dict):  # Check if the JSON data is a single dictionary
+                    df = pd.DataFrame([data])
+                    #print(df)
+                else:
+                    print(f"Unsupported JSON format in '{json_object['Key']}'")
 
+        else:
+            print(f"No objects found in the '{directory_prefix}' directory.")
+    except Exception as e:
+        print(f"Error: {e}")
 
+    #print(df_array)
