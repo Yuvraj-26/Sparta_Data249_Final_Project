a9730f529780ae76d26f2f74797600a538235dc8
Ruth Chanarin <chanarinruth@gmail.com>
2023-10-02 16:12:13 +0100

    Created main.py
    created the main.py file & writing comments to set out what needs to be added.

---
+++ main.py
@@ -0,0 +1,11 @@
+# import all necessary libraries & functions from the various modules
+
+
+# define main function, with all functions in it in ordered
+
+def main():
+    '''
+    This function runs our ELT pipeline, extracting data from sources, and 
+    loading it into a database
+    '''
+    return 
