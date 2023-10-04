f70713a2a24c04e9299dc8135e68ef69e553a424
wilkiesophie <wilkiesophie@gmail.com>
2023-10-04 12:03:00 +0100

    On branch sophie-dev  Changes to be committed: 	modified:   extract_transform_json.py

--- extract_transform_json.py
+++ extract_transform_json.py
@@ -4,11 +4,13 @@ import boto3
 
 pd.set_option('display.max_columns', None)
 
-# Function to list each tech language skill in JSON as new column with key as column label and value as cell in row
+#Funtion to list each tech languge skill in JSON as new column with key as column label and value as cell in row
 def expand_tech_self_score(df):
+
     # Check 'tech_self_score' column exists in our pre-created data frames and that it contains dictionaries
     if 'tech_self_score' in df and all(isinstance(item, dict) for item in df['tech_self_score']):
-        # Split dictionary into separate columns
+
+        #Split dictionary into separate columns
         tech_self_score_df = pd.DataFrame(df['tech_self_score'].tolist())
 
         # Add the new columns to pre-existing data frame (now languages are last columns)
@@ -18,13 +20,12 @@ def expand_tech_self_score(df):
         df.drop(columns=['tech_self_score'], inplace=True)
     return df
 
-
 def expand_strength_weakness(df):
     # Check strength and weakness columns exits in the pre-existing data frame
     if 'strengths' in df and all(isinstance(item, list) for item in df['strengths']):
+
         # Create separate columns for each item in list
-        strength_df = pd.DataFrame(df['strengths'].tolist(),
-                                   columns=[f'strengths {i + 1}' for i in range(len(df['strengths'][0]))])
+        strength_df = pd.DataFrame(df['strengths'].tolist(), columns=[f'strengths {i + 1}' for i in range(len(df['strengths'][0]))])
 
         # Add the new columns to the pre-existing data frame
         df = pd.concat([df, strength_df], axis=1)
@@ -33,9 +34,9 @@ def expand_strength_weakness(df):
         df.drop(columns=['strengths'], inplace=True)
 
     if 'weaknesses' in df and all(isinstance(item, list) for item in df['weaknesses']):
+
         # Create separate columns for each listy item
-        weakness_df = pd.DataFrame(df['weaknesses'].tolist(),
-                                   columns=[f'weaknesses {i + 1}' for i in range(len(df['weaknesses'][0]))])
+        weakness_df = pd.DataFrame(df['weaknesses'].tolist(), columns=[f'weaknesses {i + 1}' for i in range(len(df['weaknesses'][0]))])
 
         # Add new weakness columns to ore-existing data frame
         df = pd.concat([df, weakness_df], axis=1)
@@ -44,14 +45,12 @@ def expand_strength_weakness(df):
         df.drop(columns=['weaknesses'], inplace=True)
     return df
 
-'''
- Function to split 'name' column into 'first_name' and 'last_name'
 def split_name_column(df):
     if 'name' in df.columns:
-        df[['first_name', 'last_name']] = df['name'].str.split(' ', 1, expand=True)
-        df = df[['first_name', 'last_name'] + [col for col in df.columns if col != 'name']]
+        df[['first_name','last_name']] = df['name'].str.split(n=1, expand=True)
+        df.drop(columns=['name'], inplace=True)
     return df
-    '''
+
 
 # MAIN FUNCTION TO EXTRACT AND CONVERT JSON FILE TO DATA FRAME
 def json_df():
@@ -89,20 +88,19 @@ def json_df():
 
     return df_array
 
-
 if __name__ == "__main__":
     example_dfs = json_df()
 
-    # Call function to split tech skill score column
+    # Call function to split tech skill score column and process the DataFrames
     processed_dfs = []
     for df in example_dfs:
-        processed_df = expand_tech_self_score(df)
-        processed_df = expand_strength_weakness(processed_df)
-        processed_dfs.append(processed_df)
+        df = expand_tech_self_score(df)
+        df = expand_strength_weakness(df)
+        df = split_name_column(df)
+        processed_dfs.append(df)
 
     # Show and label each new data frame
     for i, df in enumerate(processed_dfs):
         print(f"Person {i + 1}:")
         print(df)
         print()
-
